bf8f2dc46767601ec699ef5f18fa92ceaf9716db
Mariusz Michalski <supermariusz@gmail.com>
2024-02-15 18:44:00 +0100

    chore: added css reset, fixed handling tsconfig path aliases

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom/client';
 
 import App from './App.tsx';
 
-import './index.css';
+import '@/styles/reset.css';
+import '@/styles/index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
