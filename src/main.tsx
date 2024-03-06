1b8c752fdeff4015c1b8f46b4408b2a1fd92dce0
Mariusz Michalski <supermariusz@gmail.com>
2024-03-06 08:32:54 +0100

    chore: added basic layout (grid)

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,6 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 
 import '@/styles/reset.css';
-import '@/styles/index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
