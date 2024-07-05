fde3b26383af99b8887c4726a89f1b9a906833fb
Mariusz Michalski <supermariusz@gmail.com>
2024-07-05 19:16:38 +0200

    chore: fixed tests config + linter

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,7 +7,7 @@ module.exports = {
     'plugin:react-hooks/recommended',
     'prettier'
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  ignorePatterns: ['coverage', 'dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
   plugins: [
     'prettier',
--- src/test/test-utils.tsx
+++ src/test/test-utils.tsx
@@ -1,11 +1,4 @@
-import { useAppStore } from '@/store';
 import { cleanup } from '@testing-library/react';
-
-const initialStoreState = useAppStore.getState();
-
-beforeEach(() => {
-  useAppStore.setState(initialStoreState, true);
-});
 
 afterEach(() => {
   cleanup();
